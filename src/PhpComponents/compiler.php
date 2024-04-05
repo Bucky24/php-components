ab1f9d67c63c15ff7450fb8ac903d8ebaeae3582
Robbert Wijtman <robbert@xinxmedia.com>
2024-04-05 14:33:02 -0700

    remove debug, watcher improvement

--- src/PhpComponents/compiler.php
+++ src/PhpComponents/compiler.php
@@ -1,6 +1,6 @@
 <?php
 
-$DO_LOG = true;
+$DO_LOG = false;
 
 $options = getopt("", array(
     "file:",
--- src/PhpComponents/utils/watcher.js
+++ src/PhpComponents/utils/watcher.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
 
+if (process.argv.length <= 2) {
+    console.error("USAGE: watcher.js <directory>");
+    process.exit(1);
+}
+
 const dir = process.argv[2];
 
 let fullDir = dir;
@@ -11,14 +16,18 @@ if (dir.startsWith(".")) {
 
 const compilerPath = path.resolve(__dirname, "..", "compiler.php");
 
-console.log("Watching", dir, "for changes");
-fs.watch(dir, { recursive: true }, (event, file) => {
-    const fullFile = path.join(fullDir, file);
-    //console.log(event, fullFile);
-
+function doCompile() {
     const command = `php ${compilerPath} --dir ${fullDir}`;
 
     exec(command, (err, stdout, stderr) => {
         console.log(stdout);
     });
+}
+
+console.log("Watching", dir, "for changes");
+fs.watch(dir, { recursive: true }, (event, file) => {
+    doCompile();
 });
+
+// run one right at the start to get us going
+doCompile();
